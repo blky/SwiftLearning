7864ab4191b7bcb76af4076c1b22412264118f36
blky <jademails@hotmail.com>
2014-10-19 23:11:23 -0700

    test all swift basic

---
+++ SwiftLearing/ClassRelated/LearningClass.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class SurveyQuestion {
+    var text: String
+    var response: String?
+    init(text: String) {
+        self.text = text
+    }
+    func ask() {
+        println(text)
+    }
+}
+
